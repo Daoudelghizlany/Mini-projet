5633ca8c394b6a43f290785cabb46cec57d355d5
Daoudelghizlany <daoudelghizlany@gmail.com>
2025-05-25 16:51:07 +0100

    Add files via upload

---
+++ D.java
@@ -0,0 +1,7 @@
+public class D{
+ public static viod main (String[] args ) {
+scanner s =newsystem(scanner.in); 
+syso("donner deaux nombre ");
+int a = s.nextint();
+int b = s.nextint();
+syso(a+b);
